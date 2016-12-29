5ad185f222ca380edba5bf387901fa06c80e0bd7
Nekuake <ruizale55@gmail.com>
2016-12-29 13:35:57 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,24 @@
-print("WELCOME TO THE SHIPPER, WHERE EVERYONE CAN BE SHIPPED WITH EVERYONE")
+import random
+print("BIENVENIDO AL SHIPEADOR. DONDE TODO EL MUNDO PUEDE SER SHIPEADO CON LO QUE SEA")
+
+persona2int = random.randomint(1,18)
+persona1 = 0
+persona2 = 0
+while persona1 == 0:
+  persona1int = random.randomint(1,18)
+  if persona1int == 1:
+    persona1 = "Dalas"
+  elif persona1int == 2:
+    persona1 = "Aguirre"
+  elif persona1int == 3:
+    persona1 = "Santiago Abascal ESPAÑOL"
+  elif persona1int == 4:
+    persona1 = "el entrenador Pokémon neoliberal"
+  elif persona1int == 5:
+    persona1 = "tu amigo furry que retwittea YIFF"
+  elif persona1int == 6:
+    persona1 = "JUAN DEMONIO"
+  elif persona1int == 7:
+    persona1 = "uno de los bots que te siguen"
+  elif persona1int == 8:
+    
